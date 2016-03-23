9b7a2d58a4a9a939377bcbf791bf66785180ad54
zhaoweijin <zhaoweijin@appgame.com>
2016-03-23 17:39:26 +0800

    no message

--- data_optimum_run/project/tests/test.py
+++ data_optimum_run/project/tests/test.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+
+import sys
+import os
+
+# Insert project root path to sys.path
+project_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if project_path not in sys.path:
+    sys.path.insert(0, project_path)
+
 import time
 import MySQLdb
+
 from config import load_config
 
 config = load_config()
