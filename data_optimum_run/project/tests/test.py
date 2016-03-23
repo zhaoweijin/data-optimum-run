9480915308111f2d0141ddd585eabb7392cf000e
zhaoweijin <zhaoweijin@appgame.com>
2016-03-23 16:39:14 +0800

    no message

--- data_optimum_run/project/tests/test.py
+++ data_optimum_run/project/tests/test.py
@@ -8,12 +8,12 @@ import sys
 import os
 
 # Insert project root path to sys.path
-project_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-if project_path not in sys.path:
-    sys.path.insert(0, project_path)
+# project_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+# if project_path not in sys.path:
+#     sys.path.insert(0, project_path)
 
-from config.development import DevelopmentConfig
-# from config import load_config
+# from config.development import DevelopmentConfig
+from config import development
 
 # ltime=time.localtime(1395025933)
 # timeStr=time.strftime("%Y-%m-%d %H:%M:%S", ltime)
