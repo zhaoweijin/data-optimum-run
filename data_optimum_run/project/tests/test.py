7edc4b59200645cbd0eb86abb3de6bbf75fa74d2
zhaoweijin <zhaoweijin@appgame.com>
2016-03-23 16:34:57 +0800

    no message

--- data_optimum_run/project/tests/test.py
+++ data_optimum_run/project/tests/test.py
@@ -3,8 +3,17 @@
 
 import time
 import MySQLdb
-# from config.development import DevelopmentConfig
-from config import load_config
+
+import sys
+import os
+
+# Insert project root path to sys.path
+project_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if project_path not in sys.path:
+    sys.path.insert(0, project_path)
+
+from config.development import DevelopmentConfig
+# from config import load_config
 
 # ltime=time.localtime(1395025933)
 # timeStr=time.strftime("%Y-%m-%d %H:%M:%S", ltime)
