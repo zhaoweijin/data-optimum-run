d80422437a60d0a0c12461536a733146127a185e
zhaoweijin <zhaoweijin@appgame.com>
2016-03-23 18:56:19 +0800

    no message

--- data_optimum_run/project/config/default.py
+++ data_optimum_run/project/config/default.py
@@ -1,4 +1,5 @@
 class Config(object):
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:123456@127.0.0.1/hello'
     DATABASE_HOST = '127.0.0.1'
     DATABASE_USER = ''
     DATABASE_PASSWORD = ''
--- data_optimum_run/project/config/development_sample.py
+++ data_optimum_run/project/config/development_sample.py
@@ -1,5 +1,6 @@
 from default import Config
 class DevelopmentConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:123456@127.0.0.1/hello'
     DATABASE_HOST = '127.0.0.1'
     DATABASE_USER = ''
     DATABASE_PASSWORD = ''
