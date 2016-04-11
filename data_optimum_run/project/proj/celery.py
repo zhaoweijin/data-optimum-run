405faeedc8f0c564a97d1471d087ecae73f8d38c
zhaoweijin <zhaoweijin@appgame.com>
2016-04-11 17:57:13 +0800

    no message

--- data_optimum_run/project/proj/celery.py
+++ data_optimum_run/project/proj/celery.py
@@ -2,10 +2,10 @@
 # -*- coding:utf-8 -*-
 
 from __future__ import absolute_import
-from celery import Celery
+from celery import Celery,platforms
 
 app = Celery('proj', include=['proj.tasks'])
-
+platforms.C_FORCE_ROOT = True
 app.config_from_object('proj.config')
 
 if __name__ == '__main__':
